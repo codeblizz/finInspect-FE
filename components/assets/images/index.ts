af09c1308ed455d8161e0e26bba28e4de2bb7f43
collins.dev <codeblizzen@gmail.com>
2023-01-17 18:27:46 +0400

    Added: Slider to login and Register screen

--- components/assets/images/index.ts
+++ components/assets/images/index.ts
@@ -1,11 +1,28 @@
-import HeroOne from './heroOne';
-import HeroTwo from './heroTwo';
-import HeroThree from './heroThree';
-import HeroFour from './heroFour';
+import HeroOne from './image1.png';
+import HeroTwo from './image2.png';
+import HeroThree from './image3.png';
+import HeroFour from './image4.png';
+import HeroFive from './image5.png';
+import HeroSix from './image6.png';
+import HeroSeven from './image7.png';
 
 export const ImageElem = [
-  { img: HeroOne },
-  { img: HeroTwo },
-  { img: HeroThree },
-  { img: HeroFour },
+  HeroOne,
+  HeroTwo,
+  HeroThree,
+  HeroFour,
+  HeroFive,
+  HeroSix,
+  HeroSeven,
+];
+
+export const ImageDesc = [
+  'Link all your bank accounts world-wide securely and see a \n full view of your finances.',
+  'Add all your assets and liabilities to get a comprehensive \n net worth report',
+  'Safely store all your financial documents and beneficiaries \n in case of an emergency.',
+  'The best security features ensure all your data is \n completely safe.',
+  'Add all your assets and liabilities to get a comprehensive \n net worth report',
+  'Safely store all your financial documents and beneficiaries \n in case of an emergency.',
+  'The best security features ensure all your data is \n completely safe.',
+  'Safely store all your financial documents and beneficiaries \n in case of an emergency.',
 ];
--- components/molecules/hero.tsx
+++ components/molecules/hero.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from '../atomic/image';
-import { ImageElem } from '../assets/images';
+import { ImageElem, ImageDesc } from '../assets/images';
 import styles from '../../styles/Login.module.css';
 import Box from '../atomic/box';
+import classNames from 'classnames';
+import { BsDot } from 'react-icons/bs';
+import Paragraph from 'components/atomic/paragragh';
 
 function Hero() {
+  const [index, setIndex] = useState(0);
+  const timeoutRef: { current: NodeJS.Timeout | null } = useRef(null);
+
+  const resetTimeout = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  };
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(() => {
+      setIndex((index + 1) % ImageElem.length);
+    }, 4000);
+    return () => {
+      resetTimeout();
+    };
+  }, [ImageElem, index]);
+
   return (
-    <Box classes={styles.loginHero}>
-      {ImageElem.map((val, index): any => (
-        <Image
-          key={index}
-          src={val.img}
-          alt='hero'
-          className=''
-          width={''}
-          height={''}
-          priority={true}
-        />
-      ))}
+    <Box
+      classes={classNames([
+        styles.loginHero,
+        '-mt-3 border-r-2 border-teal-900 flex flex-col',
+      ])}
+    >
+      <Image
+        src={ImageElem[index]}
+        alt='hero'
+        className='rounded-lg -ml-10'
+        width={''}
+        height={''}
+        priority={true}
+      />
+      <div className='flex flex-col justify-center items-center'>
+        <Paragraph className='text-xs my-2' text={ImageDesc[`${index + 1 % ImageDesc.length}`]} />
+        <div className=''>
+          <ul className='flex'>
+            {ImageElem.map((img, i) => (
+              <li
+                key={i}
+                className={`${index === i ? 'text-white' : 'text-gray-500'}`}
+              >
+                <BsDot onClick={() => setIndex(i)} className='cursor-pointer w-5 h-5'/>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </Box>
   );
 }
